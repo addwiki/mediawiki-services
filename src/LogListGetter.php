e9529732c5af6c2e3a3ff12165094ed3dbfdd5b0
Bene <benestar.wikimedia@gmail.com>
2015-09-07 12:44:31 +0200

    Add LogListGetter

---
+++ src/LogListGetter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace MediaWiki\Services;
+
+using MediaWiki\DataModel\LogList;
+
+/**
+ * @author Bene* < benestar.wikimedia@gmail.com >
+ */
+interface LogListGetter {
+
+	/**
+	 * @param string|null $type filter log entries by type
+	 * @param string|null $action filter log entries by action, overrides $type
+	 * @param int $namespace filter log entries in the given namespace
+	 *
+	 * @return LogList
+	 */
+	public function getLogList( $type = null, $action = null, $namespace = 0 );
+
+}
+
